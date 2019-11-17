8b724cbd468e3bba685181b24c50fba988ed462d
Rafael Zasas <zasas.rafi@gmail.com>
2019-11-17 13:15:03 -0800

    Added programs 2 and 3
    2 was copied from cocalc but doesn't work anyway and neither does 3.

--- Homework16-17.py
+++ Homework16-17.py
@@ -1,8 +1,10 @@
+import random
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
 # PROGRAM 1  - TUPLES
-import math, random
-from typing import Match
-
-
 def tuples():
     x = []
     y = []
@@ -19,3 +21,52 @@ def tuples():
 
 
 tuples()
+
+
+# PROGRAM 2  - CIRCLES
+
+# DONE ON COCALC BUT CURRENTLY NOT WORKING
+
+# def generate_tuples():
+#     x = []
+#     y = []
+#     for i in range(10):
+#         x.append(round(random.uniform(-1, 1), 3))
+#         y.append(round(random.uniform(-1, 1), 3))
+#     a = list(zip(x, y))
+#     return a
+#
+#
+# def circles():
+#     x = np.linspace(0, 1, 100)
+#     y = np.linspace(0, 1, 100)
+#     my_tuple = generate_tuples()
+#
+#     for tup in my_tuple:
+#         x, y = tup
+#         plt.plot(x, y)
+#
+#     plt.title("Tuple plot")
+#     plt.xlabel('x label')
+#     plt.ylabel('y label')
+#
+#     plt.show()
+#
+#
+# circles()
+
+
+# PROGRAM 3  - BRUTE FORCE TUPLE COMPARISON
+
+# ALSO CURRENTLY NOT WORKING
+
+def brute(my_tuple):
+    x_diff = 1
+    y_diff = 1
+    for tup in my_tuple:
+        x, y = tup
+        print(f'{x} {y}')
+
+
+A = [(0, 2.1), (-1.3, 4.3), (5.7, 9), (-5, 4), (2.4, 6)]
+brute(A)
